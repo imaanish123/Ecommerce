30ba99c79046c0f43dd3cba1f8658a0d1c21093b
Manish Kumar <mk1849253@gmail.com>
2025-09-07 11:52:08 +0530

    initial commit

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <h1>footer comp</h1>
+    </div>
+  );
+};
+
+export default Footer;
+
+
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+import morgan from "morgan";
 
 //config
 dotenv.config();
 
+//database config
+connectDB();
+
 // rest object
 const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(morgan("dev"));
+
+//routes
+app.use("/api/v1/auth", authRoutes);
 
 //rest api
 app.get("/", (req, res) => {
