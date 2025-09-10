c64e91167d726915bd1cc54bdb1de0d0428a0df2
Manish Kumar <mk1849253@gmail.com>
2025-09-10 17:57:15 +0530

    add pages

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -2,18 +2,18 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>footer comp</h1>
+    <div
+      className="footer"
+      style={{
+        backgroundColor: "#333",
+        color: "white",
+        padding: "10px 0",
+        height: "70px",
+      }}
+    >
+      <h1 className="text-center">All Right Reserved &copy; Techinfoyt</h1>
     </div>
   );
 };
 
 export default Footer;
-
-
-
-
-
-
-
-
