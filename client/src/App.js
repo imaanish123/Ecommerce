fee4ec65f3dc7d5009ca67cef166f5d8801c037f
Manish Kumar <mk1849253@gmail.com>
2025-09-11 01:26:23 +0530

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import HomePage from "./pages/HomePage";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Policy from "./pages/Policy";
+import Pagenotfound from "./pages/Pagenotfound";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/policy" element={<Policy />} />
-        <Route path="*" element={<h1>Page Not Found</h1>} />
+        <Route path="*" element={<Pagenotfound />} />
       </Routes>
     </>
   );
